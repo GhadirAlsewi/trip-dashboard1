121b63546d1f78da96d3b858a276a1a792f9616a
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-10-19 15:59:00 -0400

    photos

--- src/layouts/trips/AddTrip.js
+++ src/layouts/trips/AddTrip.js
@@ -1,9 +1,12 @@
-import { Card, Grid, TextField } from "@mui/material";
+import { Button, Card, Grid, Icon, IconButton, TextField } from "@mui/material";
+// import { SendIcon } from "@mui/material/";
 import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs'
+import dayjs from "dayjs";
+// import MDButton from "components/MDButton";
 const { default: DashboardLayout } = require("examples/LayoutContainers/DashboardLayout");
 
 function AddTrip() {
@@ -30,10 +33,35 @@ function AddTrip() {
                                                 renderInput={(props) => <TextField {...props} />}
                                                 label="Trip Date"
                                                 onChange={(newValue) => {
-                                                // setValue(newValue);
+                                                console.log(dayjs(newValue).format("YYYY-MM-DD HH:mm:ss"))
                                                 }}
                                             />
                                         </LocalizationProvider>
+                                        
+                                    </MDBox>
+
+                                    <MDBox mb={3}>
+                                        <Button variant='contained' color='primary'>
+                                            <MDTypography color='white' variant='p'>
+                                                <Grid container spacing={1}>
+                                                    <Grid item>
+                                                        <Icon>photo_library</Icon>
+                                                    </Grid>
+                                                    <Grid item>
+                                                        Upload Photos
+                                                    </Grid>
+                                                </Grid>
+                                            </MDTypography>
+                                            <input hidden accept='image/*' type='file' id='trip-photos' multiple />
+                                        </Button >
+                                        {/* <Button variant='contained' color='primary'>
+                                            <MDTypography color='white' variant='p'><Icon>photo_library</Icon>&nbsp;Upload Photos</MDTypography>
+                                            <input hidden accept='image/*' type='file' id='trip-photos' multiple />
+                                        </Button > */}
+                                        {/* <IconButton color='primary' aria-label='upload picture' component="label">
+                                            <input hidden accept='image/*' type='file' multiple />
+                                            <PhotoCamera />
+                                        </IconButton > */}
                                         
                                     </MDBox>
                                 </MDBox>
